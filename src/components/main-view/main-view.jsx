c444b1062913140f83d7fc88aac4086ceabc0431
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-05-18 19:32:04 -0400

    add movie descriptions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,9 +7,9 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
+        { _id: 1, Title: 'Fastastic Beasts and Where to Find Them', Director: 'David Yates', Genre: 'Fantasy', Description: 'The adventures of writer Newt Scamander in New York\'s secret community of witches and wizards seventy years before Harry Potter reads his book in school.', ImagePath: '../../img/fantasticbeasts' },
+        { _id: 2, Title: 'The Hobbit: An Unexpected Journey', Director: 'Peter Jackson', Genre: 'Fantasy', Description: 'A reluctant Hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home, and the gold within it from the dragon Smaug.', ImagePath: '../../img/thehobbit.png' },
+        { _id: 3, Title: 'Avengers: Endgame', Director: 'Anthony Russo', Genre: 'Action', Description: 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos\' actions and restore balance to the universe.', ImagePath: '../../img/avengersendgame.png' }
       ],
       selectedMovie: null
     };

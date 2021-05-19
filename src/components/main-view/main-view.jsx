f182d53aa6a31862af2b02309beebe6fc53455bb
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-05-19 13:04:20 -0400

    import images in main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+import fantasticbeastsImage from 'url:../../img/fantastic.jpg';
+import thehobbitImage from 'url:../../img/hobbit.jpg';
+import avengersendgameImage from 'url:../../img/avengers.jpg';
+
 export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Fastastic Beasts and Where to Find Them', Director: 'David Yates', Genre: 'Fantasy', Description: 'The adventures of writer Newt Scamander in New York\'s secret community of witches and wizards seventy years before Harry Potter reads his book in school.', ImagePath: '../../img/fantasticbeasts' },
-        { _id: 2, Title: 'The Hobbit: An Unexpected Journey', Director: 'Peter Jackson', Genre: 'Fantasy', Description: 'A reluctant Hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home, and the gold within it from the dragon Smaug.', ImagePath: '../../img/thehobbit.png' },
-        { _id: 3, Title: 'Avengers: Endgame', Director: 'Anthony Russo', Genre: 'Action', Description: 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos\' actions and restore balance to the universe.', ImagePath: '../../img/avengersendgame.png' }
+        { _id: 1, Title: 'Fastastic Beasts and Where to Find Them', Director: 'David Yates', Genre: 'Fantasy', Description: 'The adventures of writer Newt Scamander in New York\'s secret community of witches and wizards seventy years before Harry Potter reads his book in school.', ImagePath: fantasticbeastsImage },
+        { _id: 2, Title: 'The Hobbit: An Unexpected Journey', Director: 'Peter Jackson', Genre: 'Fantasy', Description: 'A reluctant Hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home, and the gold within it from the dragon Smaug.', ImagePath: thehobbitImage },
+        { _id: 3, Title: 'Avengers: Endgame', Director: 'Anthony Russo', Genre: 'Action', Description: 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos\' actions and restore balance to the universe.', ImagePath: avengersendgameImage }
       ],
       selectedMovie: null
     };

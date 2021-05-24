52d7682558ddff8db2fbfe90bfb1c01ff63790af
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-05-24 19:22:18 -0400

    bug correct handleSubmit = (e)

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -5,7 +5,7 @@ export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
     /* Send a request to the server for authentication */

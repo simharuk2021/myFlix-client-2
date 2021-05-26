addd1734df2081697bc013ed735ac92d9a2bae7a
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-05-26 13:11:24 -0400

    add bootstrap form and button to registrationView

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Form from 'react-bootstrap';
+import Button from 'react-bootstrap';
+
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
@@ -14,22 +17,26 @@ export function RegistrationView(props) {
   }
 
   return (
-    <form>
-      <label>Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>Password:
-        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>Email:
-        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>Birthday:
-        <input type="text" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </label>
-      <button type="button" onClick={handleSubmit}>Submit</button>
-      <button onClick={() => { onBackClick(null); }}>Back</button>
-    </form>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username: </Form.Label>
+        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password: </Form.Label>
+        <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Form.Group controlId="formEmail">
+        <Form.Label>Email: </Form.Label>
+        <Form.Control type="text" onChange={e => setEmail(e.target.value)} />
+      </Form.Group>
+      <Form.Group controlId="formBirthday">
+        <Form.Label>Birthday: </Form.Label>
+        <Form.Control type="text" onChange={e => setBirthday(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="button" onClick={handleSubmit}>Submit</Button>
+      <Button variant="secondary" onClick={() => { onBackClick(null); }}>Back</Button>
+    </Form>
   )
 }
 

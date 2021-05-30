dbe57f4bcace07ada318e32ebce2463bfb78cb91
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-05-30 13:56:24 -0400

    import scss files for components

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+import './registration-view.scss';
+
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');

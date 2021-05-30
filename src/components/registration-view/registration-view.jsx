6ad776cea903a8b762a5233fffa82e6659535ed6
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-05-30 15:49:42 -0400

    add logo to registrationView

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './registration-view.scss';
 
@@ -19,26 +21,34 @@ export function RegistrationView(props) {
   };
 
   return (
-    <Form id="reg-form">
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username: </Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password: </Form.Label>
-        <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formEmail">
-        <Form.Label>Email: </Form.Label>
-        <Form.Control type="text" onChange={e => setEmail(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formBirthday">
-        <Form.Label>Birthday: </Form.Label>
-        <Form.Control type="text" onChange={e => setBirthday(e.target.value)} />
-      </Form.Group>
-      <Button variant="outline-warning" type="button" onClick={handleSubmit}>Submit</Button> {''} {''}
-      <Button variant="outline-light" onClick={props.toggleRegister}>Back</Button>
-    </Form>
+    <div id="reg-page">
+      <h3 className="logo">myFlix</h3>
+      <Row className="justify-content-md-center">
+        <Col md={6}>
+          <Form id="reg-form">
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username: </Form.Label>
+              <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password: </Form.Label>
+              <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email: </Form.Label>
+              <Form.Control type="text" onChange={e => setEmail(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formBirthday">
+              <Form.Label>Birthday: </Form.Label>
+              <Form.Control type="text" onChange={e => setBirthday(e.target.value)} />
+            </Form.Group>
+            <Button variant="outline-warning" type="button" onClick={handleSubmit}>Submit</Button> {''} {''}
+            <Button variant="outline-light" onClick={props.toggleRegister}>Back</Button>
+          </Form>
+        </Col>
+      </Row>
+
+    </div>
   )
 }
 

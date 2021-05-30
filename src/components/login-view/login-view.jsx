04da908153d418c72d70045359895cd0aafb459e
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-05-30 15:50:43 -0400

    add logo to loginView

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './login-view.scss';
+import { NavbarBrand } from 'react-bootstrap';
 
 export function LoginView(props) {
 
@@ -21,17 +24,29 @@ export function LoginView(props) {
   };
 
   return (
-    <Form id="login-form">
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username: </Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password: </Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="outline-warning" type="submit" onClick={handleSubmit}>Submit</Button>{''} {''}
-      <Button variant="outline-light" type="button" onClick={props.toggleRegister}>Register</Button>
-    </Form>
+
+    <div id="login-page">
+      <h3 className="logo">myFlix</h3>
+
+      <Row className="justify-content-md-center">
+        <Col md={6}>
+          <Form id="login-form">
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username: </Form.Label>
+              <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password: </Form.Label>
+              <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="outline-warning" type="submit" onClick={handleSubmit}>Submit</Button>{''} {''}
+            <Button variant="outline-light" type="button" onClick={props.toggleRegister}>Register</Button>
+          </Form>
+        </Col>
+      </Row>
+
+
+
+    </div>
   );
 }

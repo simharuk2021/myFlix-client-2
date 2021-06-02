f044e8c72c33f4550f95f78aac9c91fb3a7e7629
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-02 18:11:16 -0400

    add onLoggedIn & getMovies auth
    onLoggedIn - keep authData in localStorage, call this.getMovies method
    getMovies: GET request /movies w header auth

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -41,11 +41,31 @@ export class MainView extends React.Component {
     });
   }
 
-  //when user successfully logs in - function updates 'user' property in state to that particular user//
-  onLoggedIn(user) {
+  //when user successfully logs in - function updates 'user' property in state to that particular user - keep authData in localStorage//
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user
+      user: authData.user.Username
     });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
+  }
+
+  getMovies(token) {
+    axios.get('https://myflix-movie-api-2312.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   //when user successfully regsiters - function updates 'user property in state to that partcular user//

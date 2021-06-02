8470d5a4d840c7a1af6aa890a73eb06ba13ccefa
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-02 18:49:54 -0400

    Link to MovieView from card View

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from "react-router-dom";
+
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
     return (
       <Card id="card" className="movie-card mb-2" text="white">
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title id="card-title">{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={onMovieClick} variant="outline-light">Open</Button>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link">Open</Button>
+          </Link>
         </Card.Body>
       </Card>
     );

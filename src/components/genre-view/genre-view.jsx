587225355514390f2639dd764ccf850cf36aa12e
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-03 16:10:56 -0400

    add GenreView card

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+import { Link } from "react-router-dom";
+
+import './genre-view.scss';
+
+export class GenreView extends React.Component {
+
+  render() {
+    const { genre, onBackClick } = this.props;
+
+    return (
+      <Card id="lg-card" className="genre-view">
+        <Card.Header id="movie-title">{genre.Name}</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            Description: {genre.Description} <br />
+          </Card.Text>
+          <Button variant="outline-light" onClick={onBackClick}>Back</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+GenreView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string
+    }).isRequired,
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
+

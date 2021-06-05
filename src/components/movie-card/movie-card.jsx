aa34589177848e072ed33b72e0a8ea092643b258
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-05 19:19:16 -0400

    add handleAdd function to add favorite and heart img

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import { Link } from "react-router-dom";
 
+import heartImg from 'url:../../img/heart.svg';
+
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
+  handleAdd() {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+
+    axios.post(`https://myflix-movie-api-2312.herokuapp.com/users/${user}` + "/favorites/" +
+      this.props.movie._id, {},
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then((response) => {
+        console.log(response);
+        alert(this.props.movie.Title + " has been added to your Favorites List.");
+      })
+  }
+
   render() {
     const { movie } = this.props;
     return (
@@ -17,8 +34,18 @@ export class MovieCard extends React.Component {
           <Card.Title id="card-title">{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
           <Link to={`/movies/${movie._id}`}>
-            <Button variant="outline-light">Open</Button>
+            <Button id="openMovie" variant="outline-light">Open</Button>
           </Link>
+          <button type="button" class="btn btn-white btn-rounded mr-md-3 z-depth-1a">
+            <img id="heart-img"
+              alt=""
+              src={heartImg}
+              width="20"
+              height="20"
+              fluid="true"
+              onClick={() => this.handleAdd(movie)}
+            /></button>
+
         </Card.Body>
       </Card>
     );

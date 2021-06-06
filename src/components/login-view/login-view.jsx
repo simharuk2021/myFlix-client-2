88ac3e7afdbd814fed817d2e5bb03330cb9d05fc
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-06 10:46:24 -0400

    add formValidation to loginView

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -15,22 +15,46 @@ export function LoginView(props) {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
+    let setisValid = formValidation();
+    // console.log(username, password);
     /* Send a request to the server for authentication */
-    axios.post('https://myflix-movie-api-2312.herokuapp.com/login', {
-      Username: username,
-      Password: password
-    })
-      .then(res => {
-        const data = res.data;
-        props.onLoggedIn(data);
+
+    if (setisValid) {
+      axios.post('https://myflix-movie-api-2312.herokuapp.com/login', {
+        Username: username,
+        Password: password
       })
-      .catch(e => {
-        console.log('no such user')
-      });
+        .then(res => {
+          const data = res.data;
+          props.onLoggedIn(data);
+        })
+        .catch(e => {
+          console.log('no such user')
+        });
+    };
+  }
+
+  const formValidation = () => {
+    let usernameError = {};
+    let passwordError = {};
+    let isValid = true;
+
+    if (username.trim().length < 5) {
+      usernameError.usernameShort = "Username be alphanumeric characters only and contains at least 5 characters";
+      isValid = false;
+    }
+    if (password.trim().length < 3) {
+      passwordError.passwordMissing = "You must enter a password.(minimum 4 characters) ";
+      isValid = false;
+    }
+    setUsernameError(usernameError);
+    setPasswordError(passwordError);
+    return isValid;
   };
 
   return (
@@ -44,10 +68,24 @@ export function LoginView(props) {
             <Form.Group controlId="formUsername">
               <Form.Label>Username: </Form.Label>
               <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+              {Object.keys(usernameError).map((key) => {
+                return (
+                  <div key={key} style={{ color: "red" }}>
+                    {usernameError[key]}
+                  </div>
+                );
+              })}
             </Form.Group>
             <Form.Group controlId="formPassword">
               <Form.Label>Password: </Form.Label>
               <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+              {Object.keys(passwordError).map((key) => {
+                return (
+                  <div key={key} style={{ color: "red" }}>
+                    {passwordError[key]}
+                  </div>
+                );
+              })}
             </Form.Group>
             <Button variant="outline-warning" type="submit" onClick={handleSubmit}>Submit</Button>{''} {''}
             <Link to={`/register`}>

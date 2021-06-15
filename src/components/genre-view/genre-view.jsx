7caa4127041fb41b9a9fa72f8682755b5ac464ca
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-15 18:28:13 -0400

    adjust proptypes

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -5,40 +5,29 @@ import Button from 'react-bootstrap/Button';
 
 import './genre-view.scss';
 
-export class GenreView extends React.Component {
+export function GenreView(props) {
 
-  render() {
-    const { genre, onBackClick } = this.props;
 
-    return (
-      <Card id="lg-card" className="genre-view">
-        <Card.Header id="movie-title">{genre.Name}</Card.Header>
-        <Card.Body>
-          <Card.Text>
-            {genre.Description} <br />
-          </Card.Text>
-          <Button variant="outline-light" onClick={onBackClick}>Back</Button>
-        </Card.Body>
-      </Card>
-    );
-  }
+  const { genre, onBackClick } = props;
+
+  return (
+    <Card id="lg-card" className="genre-view">
+      <Card.Header id="movie-title">{genre.Name}</Card.Header>
+      <Card.Body>
+        <Card.Text>
+          {genre.Description} <br />
+        </Card.Text>
+        <Button variant="outline-light" onClick={onBackClick}>Back</Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
+
 GenreView.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }).isRequired,
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string
-    }).isRequired,
-    ImagePath: PropTypes.string.isRequired
+  genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
   }).isRequired,
   onBackClick: PropTypes.func.isRequired
 };

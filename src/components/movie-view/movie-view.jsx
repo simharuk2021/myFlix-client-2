a8acc301297450a820dc666a04c57d649200c00d
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-06-15 18:25:45 -0400

    set movie and onBackClick as props

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -10,47 +10,25 @@ import { Link } from "react-router-dom";
 
 import './movie-view.scss';
 
-export class MovieView extends React.Component {
+export function MovieView(props) {
+  const { movie, onBackClick } = props;
 
-  render() {
-    const { movie, onBackClick } = this.props;
-
-    return (
-      <Card id="lg-card" className="movie-view">
-        <Card.Header id="movie-title">{movie.Title}</Card.Header>
-        <Card.Img className="movie-poster" variant="top" src={movie.PosterPath} />
-        <Card.Body>
-          <Card.Text>
-            Description: {movie.Description}
-            <br />
-            Genre: <Link to={`/genres/${movie.Genre.Name}`} id="link">{movie.Genre.Name}</Link>
-            <br />
-            Director:  <Link to={`/directors/${movie.Director.Name}`} id="link">{movie.Director.Name}</Link>
-            <br />
-          </Card.Text>
-          <Button id="backButton" variant="outline-light" onClick={onBackClick}>Back</Button>
-        </Card.Body >
-      </Card >
-    );
-  }
+  return (
+    <Card id="lg-card" className="movie-view">
+      <Card.Header id="movie-title">{movie.Title}</Card.Header>
+      <Card.Img className="movie-poster" variant="top" src={movie.PosterPath} />
+      <Card.Body>
+        <Card.Text>
+          {movie.Description}
+          <br />
+          Genre: <Link to={`/genres/${movie.Genre.Name}`} id="link">{movie.Genre.Name}</Link>
+          <br />
+          Director:  <Link to={`/directors/${movie.Director.Name}`} id="link">{movie.Director.Name}</Link>
+          <br />
+        </Card.Text>
+        <Button id="backButton" variant="outline-light" onClick={onBackClick}>Back</Button>
+      </Card.Body >
+    </Card >
+  );
 }
-
-MovieView.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }).isRequired,
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string
-    }).isRequired,
-    ImagePath: PropTypes.string.isRequired
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired
-};
 

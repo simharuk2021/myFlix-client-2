56dd554630ad38edba8a331d7b083e03947ebf26
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-08-25 10:38:29 -0400

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,7 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
+export const SET_REQUEST = 'SET_REQUEST';
 
 export function setMovies(value) {
   console.log('SET_MOVIES action reached');
